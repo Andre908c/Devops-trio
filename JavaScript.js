416843c62f3357934f01695fe2e66e16790849b2
andres908c <andres908c@gmail.com>
2026-08-25 22:15:25 -0500

    agregué JavaScript.js y renombré el CSS

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+const document = require('document');
+document.getElementById('myButton').addEventListener('click', function() {
+    console.log('Button clicked!');
+});
+
+const myVariable = 42; 
+myVariable += 8;
+console.log(myVariable);
+
+const myArray = [1, 2, 3, 4, 5];
+console.log(myArray);
